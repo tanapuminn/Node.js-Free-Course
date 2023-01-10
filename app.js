d521de9153296da4da392c16c0f674a1483d69d1
tanapuminn <tanapumin.fo@gmail.com>
2023-01-10 16:37:55 +0700

    s

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ productRouter.route('/').get((req,res)=> {
     )
 })
 
-productRouter.route('/1').get((req,res)=> {
-    res.send("This product 1!!")
+productRouter.route('/:id').get((req,res)=> {
+    const id = req.params;
+    res.send("This product "+id)
 })
 
 app.use("/products", productRouter)
