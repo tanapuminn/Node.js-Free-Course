cf07f9cdb312d1a49bf5784f137b1256ed4c3ead
tanapuminn <tanapumin.fo@gmail.com>
2023-01-10 15:14:11 +0700

    add route

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const app = express();
 const port = 3000;
 
 const path = require('path')
-
+const productRouter = express.Router()
 
 app.use(express.static(path.join(__dirname,'/public/')))
 
 app.set("views","./src/views")
 app.set("view engine","ejs")
+
+productRouter.route('/').get((req,res)=> {
+    res.send('This is Product')
+})
+
+productRouter.route('/1').get((req,res)=> {
+    res.send("This product 1!!")
+})
+
+app.use('/products', productRouter)
 
 app.get('/',(req,res) => {
     res.render('index',{username: 'tanapumin'})
