6934ce97396fd1811d5531d7eb16164ed11d4bda
tanapuminn <tanapumin.fo@gmail.com>
2023-01-10 16:24:50 +0700

    a

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 
 const path = require('path')
 const productRouter = express.Router()
+const products = require('./data/products.json')
 
 app.use(express.static(path.join(__dirname,'/public/')))
 
@@ -11,21 +12,16 @@ app.set("views","./src/views")
 app.set("view engine","ejs")
 
 productRouter.route('/').get((req,res)=> {
-    res.render('products',{
-        products: [
-            {tTitle: 'น้ำยาล้างจาน', Description: 'น้ำยาล้างจานสูตร 1 ขจัดคราบ', Price: 45},
-            {tTitle: 'น้ำยาล้างจาน 2', Description: 'น้ำยาล้างจานสูตร 2 ล้างมัน', Price: 30},
-            {tTitle: 'น้ำยาล้างจาน 3', Description: 'น้ำยาล้างจานสูตร 3 หอมใส', Price: 50},
-            {tTitle: 'น้ำยาล้างจาน 4', Description: 'น้ำยาล้างจานสูตร 4  มันวาว', Price: 55},
-        ]
-    })
+    res.render('products',
+        products,
+    )
 })
 
 productRouter.route('/1').get((req,res)=> {
     res.send("This product 1!!")
 })
 
-app.use('/products', productRouter)
+app.use("/products", productRouter)
 
 app.get('/',(req,res) => {
     res.render('index',{username: 'tanapumin'})
